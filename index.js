e36881bb0adc679b73482314ad0deec8d9574c00
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-08-06 13:12:47 -0700

    prehistoric search works

--- index.js
+++ index.js
@@ -30,8 +30,27 @@ app.get("/dinosaurs", (req, res) => {
     res.render("dinosaurs/index", {myDino: dinoData});
 })
 
+app.get("/prehistoric_creatures", (req, res) => {
+    let prehistoricCreatures = fs.readFileSync("./prehistoric_creatures.json");
+    let prehistoricData = JSON.parse(prehistoricCreatures);
+    console.log(prehistoricData);
+
+    let typeFilter = req.query.typeFilter;
+    if (typeFilter) {
+        prehistoricData = prehistoricData.filter((creature) => {
+            return creature.type.toLowerCase() === typeFilter.toLowerCase();
+        })
+    }
+
+    res.render("prehistoric_creatures/index", {myCreature: prehistoricData});
+})
+
 app.get("/dinosaurs/new", (req, res) => {
     res.render("dinosaurs/new");
+})
+
+app.get("/prehistoric_creatures/new", (req, res) => {
+    res.render("prehistoric_creature/new");
 })
 
 app.get("/dinosaurs/:id", (req, res) => {
@@ -50,6 +69,16 @@ app.post("/dinosaurs", (req, res) => {
     fs.writeFileSync("./dinosaurs.json", JSON.stringify(dinoData));
     // send back to /dinosaurs page at end
     res.redirect("/dinosaurs");
+})
+
+app.post("/prehistoric_creatures", (req, res) => {
+    let creatures = fs.readFileSync("./prehistoric_creatures.json");
+    let creatureData = JSON.parse(creatures);
+    creatureData.push(req.body);
+    // JSON stringify undoes JSON parse
+    fs.writeFileSync("./prehistoric_creatures.json", JSON.stringify(dinoData));
+    // send back to /prehistoric page at end
+    res.redirect("/prehistoric_creatures");
 })
 
 app.listen(PORT, () => {
