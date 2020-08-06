3ca5d1322e162ea342299ea946fc9e0acea79e4f
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-08-06 10:12:39 -0700

    home page whats popping

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = 4200;
 app.set("view engine", "ejs");
 app.use(ejsLayouts);
 
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
 app.listen(PORT, () => {
     console.log(`lisenting to port #${PORT}`);
 });
